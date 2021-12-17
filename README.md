b0840f1df64806666075ffdd432ab078c78fced9
jmoore <josh@glencoesoftware.com>
2021-12-17 14:34:34 +0100

    Add resources and build

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zarr Logo
+
+## SVGs
+
+### Horizontal
+<img src="zarr-pink-horizontal.svg" alt="horizontal version of the zarr logo" style="width:200px"/>
+
+### Stacked
+<img src="zarr-pink-stacked.svg" alt="stacked version of the zarr logo" style="width:200px"/>
+
+### Gradient
+This is the color gradient that is used in the logo.
+<img src="zarr-gradient.svg" alt="background gradient for zarr logo" style="width:200px"/>
--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #! /bin/sh
 
-find * -name *.svg -exec sh -c 'convert -background none $1 "${1%.*}.png"' sh {} \;
-mkdir -p target
-zip -r target/zarr-logos.zip *.png
+set -e
+set -u
+set -x
+
+mkdir -p target/pngs
+
+convert_each(){
+    convert -background none ${1}.svg target/pngs/${1}-transparent.png
+    convert -background white ${1}.svg target/pngs/${1}-on-white.png
+    convert -background black ${1}.svg target/pngs/${1}-on-black.png
+}
+
+convert_each zarr-pink-horizontal
+convert_each zarr-pink-stacked
+
+# And convert the gradient as is
+convert zarr-gradient-2.svg target/pngs/zarr-gradient.png
+
+cd target/pngs
+zip -r ../zarr-logos.zip *.png
